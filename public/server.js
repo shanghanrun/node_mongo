6b793c16f6db0ab09e170b7f8dcd82212d6bb675
shanghanrun <120538041+shanghanrun@users.noreply.github.com>
2023-04-11 00:12:05 +0900

    login 추가

--- public/server.js
+++ public/server.js
@@ -105,3 +105,47 @@ app.put('/edit', function(요청, 응답){
 })
 
 
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session')
+app.use(session({secret: '비밀코드', resave: true, saveUninitialized: false}))
+app.use(passport.initialize());
+app.use(passport.session())
+
+app.get('/login',function(요청,응답){
+  응답.render('login.ejs')
+})
+
+app.post('/login',passport.authenticate('local',{failureRedirect:'/fail'}), function(요청,응답){
+  응답.redirect('/')
+})
+
+app.get('/fail', function(요청, 응답){
+  응답.render('fail.ejs')
+})
+
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)  // 모든게 다 맞음
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+
+passport.serializeUser(function(user,done){
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(아이디, done){
+  done(null, {})
+})
